3346ec6c7e9aa42f331cf7bdc5198b30b7743af1
Mojtaba Eslami <mojtabaeslami.72@gmail.com>
2025-08-18 23:44:19 +0330

    feat(miniapp): generate React app with Nx

---
+++ apps/miniapp/src/app/app.tsx
@@ -0,0 +1,11 @@
+import './app.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="miniapp" />
+    </div>
+  );
+}
+export default App;
