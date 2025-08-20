2eedb5141a0008571ed6d4c37b1a9b6d1a1e6853
Mojtaba Eslami <mojtabaeslami.72@gmail.com>
2025-08-20 11:50:31 +0330

    chore: add https for local development

--- apps/miniapp/vite.config.ts
+++ apps/miniapp/vite.config.ts
@@ -2,19 +2,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import tailwindcss from '@tailwindcss/vite';
+import mkcert from 'vite-plugin-mkcert';
 
-export default defineConfig(() => ({
+const basePlugins = [react(), tailwindcss()];
+const developmentPlugins = [...basePlugins, mkcert()];
+
+export default defineConfig(({ mode }) => ({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/apps/miniapp',
   server: {
     port: 4200,
-    host: 'localhost',
+    host: '127.0.0.1',
   },
-  preview: {
-    port: 4200,
-    host: 'localhost',
-  },
-  plugins: [react(), tailwindcss()],
+  plugins: mode === 'development' ? developmentPlugins : basePlugins,
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
